0abf8897cfe8492b02b1ac9abf867370d996cad9
Sergey Dimitriev <serdim@MacBook-Pro-Sergey.local>
2019-06-17 21:30:55 +0400

    first commit

--- SecondProject/ViewController.swift
+++ SecondProject/ViewController.swift
@@ -9,10 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var TestButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        var ScreenHeight = UIScreen.main.bounds.size.height
+        var ScreenWidth = UIScreen.main.bounds.size.width
+        
+        var test = ScreenHeight*0.333333
+        
+        TestButton.frame.size.height = test
+        TestButton.frame.size.width = ScreenWidth
+        TestButton.backgroundColor = UIColor.darkGray
+        
+        
+
+        NSLayoutConstraint(item: TestButton, attribute: NSLayoutConstraint.Attribute.centerY, relatedBy: NSLayoutConstraint.Relation.equal, toItem: view, attribute: NSLayoutConstraint.Attribute.centerY, multiplier: 1, constant: 0).isActive = true
+        NSLayoutConstraint(item: TestButton, attribute: NSLayoutConstraint.Attribute.height, relatedBy: NSLayoutConstraint.Relation.equal, toItem: nil, attribute: NSLayoutConstraint.Attribute.notAnAttribute, multiplier: 1, constant: test).isActive = true
+        
     }
 
 
